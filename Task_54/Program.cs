372d154a67f9e01bf80c8d7348c0e92ff6524152
Evgenia S <Dla-evgenii@yandex.ru>
2022-10-31 21:18:24 +0600

    Задача 54 (двумерный массив с упорядочиными по убыванию элементами)

---
+++ Task_54/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+// каждой строки двумерного массива.
+
+void FillArrayRandom(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void OrderDescending(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    int temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+int[,] array = new int[5, 5];
+FillArrayRandom(array);
+PrintArray(array);
+Console.WriteLine();
+OrderDescending(array);
+PrintArray(array);
