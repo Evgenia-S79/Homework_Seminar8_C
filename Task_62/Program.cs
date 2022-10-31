67afaba771be6d063b350fb253ccad97dfa31327
Evgenia S <Dla-evgenii@yandex.ru>
2022-10-31 21:24:37 +0600

    Задача 62 (Массив 4х4, заполненный спирально)

---
+++ Task_62/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая заполнит спирально массив 4 на 4. 
+
+int[,] GetSpire(int[,] arr)
+{
+    int temp = 1;
+    int i = 0;
+    int j = 0;
+    while (temp <= arr.GetLength(0) * arr.GetLength(1))
+    {
+        arr[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < arr.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= arr.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > arr.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = new int[4, 4];
+GetSpire(array);
+PrintArray(array);
